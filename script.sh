a310a73d6b3d82dc4f43a6a01d994d1cabd36510
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-03-08 20:51:54 +0530

    Delete script.sh

--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform init
-sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform -y apply

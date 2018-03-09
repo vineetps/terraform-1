c5de356cfb3073b0ec600e80d1e7e9a0671e6382
sohammondal <soham.pf.mondal@gmail.com>
2018-03-09 16:59:43 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {
- client_id = "{{env `TF_VAR_client_id`}}"
- subscription_id = "{{env `TF_VAR_subscription_id`}}"
- client_secret = "{{env `TF_VAR_client_secret`}}"
- tenant_id = "{{env `TF_VAR_tenant_id`}}"
+ client_id = "${var.TF_VAR_client_id}"
+ subscription_id = "${var.TF_VAR_subscription_id}"
+ client_secret = "${var.TF_VAR_client_secret}"
+ tenant_id = "${var.TF_VAR_tenant_id}"
 }
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "TF_VAR_client_id" {
+  description=" "
+}
+
+variable "TF_VAR_subscription_id" {
+  description=" "
+}
+
+variable "TF_VAR_client_secret" {
+  description=" "
+}
+
+variable "TF_VAR_tenant_id" {
+  description=" "
+}

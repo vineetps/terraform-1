a3f898303eaff494bf8a181dc325fd2a7a8b9c4a
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-03-11 00:34:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
  #!/bin/sh
 
-sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform init
-sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform apply -input=false -auto-approve
-sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform show > output.txt
+
+echo "******Terraform Init*****"
+#cd workspace/TerraformInit/
+sudo wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_386.zip
+sudo unzip terraform_0.11.3_linux_386.zip
+sudo ./terraform init
+
+#sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform init
+#sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform apply -input=false -auto-approve
+#sudo /tmp/tools/org.jenkinsci.plugins.terraform.TerraformInstallation/Terraform/terraform show > output.txt

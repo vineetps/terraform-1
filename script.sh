8c52e90ed74ca4eb89525a2ae071e9fe2020f430
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-03-11 00:38:35 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,8 @@
 
 
 echo "******Terraform Init*****"
-#cd workspace/TerraformInit/
+pwd
+cd workspace/TerraformInit/
 sudo wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_386.zip
 sudo unzip terraform_0.11.3_linux_386.zip
 sudo ./terraform init

d157b7b460c54d0ab49b5d163a5da4a228f28c13
sohammondal <34343502+sohammondal@users.noreply.github.com>
2018-03-12 17:15:46 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
  #!/bin/sh
 uname -a
-apt-get --yes install sudo
+#apt-get --yes install sudo
 echo "******Terraform Init******"
 pwd
 echo "~~~~~~~~~~~~~~~~~~~~~Logged User~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
@@ -8,7 +8,7 @@ id
 #cd workspace/TerraformInit/
 wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_386.zip -P /tmp
 unzip /tmp/terraform_0.11.3_linux_386.zip
-sudo cp /tmp/terraform /usr/bin
+cp /tmp/terraform /usr/bin
 terraform init
 terraform apply 
 

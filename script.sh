35d6c13f78c2e568c36b24251f8552836713e8ab
sohammondal <soham.pf.mondal@gmail.com>
2018-03-13 11:34:36 +0530

    added comments to script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,26 @@
 #!/bin/sh
 
 
-echo "******Terraform Init******"
-
-
-
+echo "************************************************"
+echo '                Terraform Download              '
+echo "************************************************"
 
 wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_386.zip
 
 unzip -o /tmp/terraform_0.11.3_linux_386.zip
 
+echo "************************************************"
+echo '                Terraform Init                 '
+echo "************************************************"
+echo -e "\n"
+
 ./terraform init
+
+
+echo "************************************************"
+echo '                Terraform Apply                 '
+echo "************************************************"
+echo -e "\n"
 
 ./terraform apply -auto-approve -var client_id=${client_id} -var client_secret=${client_secret} -var subscription_id=${subscription_id} -var tenant_id=${tenant_id}
 

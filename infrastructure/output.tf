6813790f12a356363076837050a51496c701a9fd
sohammondal <soham.pf.mondal@gmail.com>
2018-03-15 16:46:04 +0530

    added public ip address in output.tf

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,15 +1,21 @@
 ## Network Interface
 
 output "Network_Interface" {
-value = "${azurerm_network_interface.networkinterface.id}"
+  value = "${azurerm_network_interface.networkinterface.id}"
 }
 
 ## Resource Group
 
 output "Resourrce_Group_Location" {
-value = "${azurerm_resource_group.rg.location}"
+  value = "${azurerm_resource_group.rg.location}"
 }
 
 output "Resourrce_Group_Name" {
-value = "${azurerm_resource_group.rg.name}"
+  value = "${azurerm_resource_group.rg.name}"
+}
+
+##Public IP
+
+output "Public_IP" {
+  value = "${azurerm_public_ip.eip.ip_address}"
 }

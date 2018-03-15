45c922be66a0c1a02bb1d5baa7f391e411e00ee7
sohammondal <soham.pf.mondal@gmail.com>
2018-03-15 17:32:47 +0530

    literal error

--- vm/vm.tf
+++ vm/vm.tf
@@ -13,7 +13,7 @@ resource "azurerm_managed_disk" "disk" {
 
 resource "azurerm_virtual_machine" "vm" {
   name                  = "ChefLinux-VM"
-  location              = "${var."
+  location              = "${var.Resource_Group_Name}"
   resource_group_name   = "${var.Resource_Group_Name}"
   network_interface_ids = ["${var.Network_Interface}"]
   vm_size               = "Standard_DS1_v2"
